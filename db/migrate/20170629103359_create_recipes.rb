740a2592913800448e48da23b57e4c92a15d8944
Param Krish <mkparam@gmail.com>
2017-07-04 19:36:20 +0530

    db migration adjustment

--- db/migrate/20170629103359_create_recipes.rb
+++ db/migrate/20170629103359_create_recipes.rb
@@ -1,10 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration[5.1]
   def change
-  create_table "recipes" do |t|
-    t.text "ingredients"
-    t.text "procedure"
-    t.string "item"
-  end
-
-
+	  create_table "recipes" do |t|
+	    t.text "ingredients"
+	    t.text "procedure"
+	    t.string "item"
+	  end
+	end
 end
